38f331018fa5c2b9deecec9445a240811f1b2a64
setyo <tyoalvino135@gmail.com>
2022-08-23 10:57:10 +0700

    first commit

---
+++ backend/models/UserModel.js
@@ -0,0 +1,20 @@
+import sequelize from 'sequelize';
+import db from '../config/Database.js';
+
+const { DataTypes } = sequelize;
+
+const User = db.define(
+    'users', {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        gender: DataTypes.STRING,
+    }, {
+        freezeTableName: true,
+    }
+);
+
+export default User;
+
+(async() => {
+    await db.sync();
+})();
